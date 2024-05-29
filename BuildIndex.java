d75bb9f3cce31b1483458448042f83f76b488945
許育瑋 <blackfire106.1231@gmail.com>
2024-05-29 20:28:07 +0800

    Save word Array data

--- BuildIndex.java
+++ BuildIndex.java
@@ -9,25 +9,17 @@ import java.io.File;
 
 public class BuildIndex {
     
-    public static ArrayList<Trie> docSet = new ArrayList<Trie>();
     public static Trie document = new Trie();
     public static long startTime = System.currentTimeMillis();
 
     public static void main(String[] args) {
-        docSet = getDocString(args[0]);
-        System.out.println(docSet.get(0).totalWords);
         
-        //build dir 
-        String path = "data_dir";
-        File singleDir = new File(path);
-        if (singleDir.exists())
-            deleteDirectory(singleDir);//delete old data
-        singleDir.mkdir();
-        
+        getDocString(args[0]);
 
-        document.toSerial(String.format("%s/%s", path, "BigTrie"));
-        for (int i = 0;i < docSet.size();i++)
-            docSet.get(i).toSerial(String.format("%s/%d", path, i));
+        System.out.println(document.search("is", 4));
+        System.out.println(document.totalWords.get(4));
+        
+        document.toSerial(args[0].substring(0, args[0].length() - 4));
 
     }
 
@@ -44,7 +36,7 @@ public class BuildIndex {
         return lines;
     }
 
-    public static ArrayList<Trie> getDocString(String fileName) {
+    public static void getDocString(String fileName) {
         ArrayList<String> lines = readLine(fileName);
 
         
@@ -53,36 +45,17 @@ public class BuildIndex {
             String newLine = lines.get(i).toLowerCase().replaceAll("[^a-z]", " ");
             String[] words = newLine.split(" ");
 
-
-            if ((i / 5) + 1 > docSet.size())
-                docSet.add(new Trie());
+            int index = (i / 5);
 
             for (String word : words) {
                 if (word.length() == 0)
                     continue;
 
-                if (docSet.get(i / 5).search(word) == 0){
-                    document.insert(word, 1);
-                }
-
-                docSet.get(i / 5).insert(word, 1);
+                document.insert(word, 1, index);
             }
         }
-        return docSet;
     }
-    private static void deleteDirectory(File directory) {
-        File[] files = directory.listFiles();
-        if (files != null) { // Ensure it's not null to avoid NullPointerException
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    deleteDirectory(file); // Recursive call for sub-directory
-                } else {
-                    file.delete();
-                }
-            }
-        }
-        directory.delete();
-    }
+    
 }
 
 
--- Indexer.java
+++ Indexer.java
@@ -2,22 +2,53 @@
 import java.io.IOException;
 import java.io.Serializable;
 import java.io.FileOutputStream;
-import java.io.FileInputStream;
-import java.io.DataOutputStream;
-import java.io.DataInputStream;
+import java.io.ObjectOutputStream;
 import java.io.File;
+import java.util.ArrayList; 
 
-public class Indexer implements Serializable {
-    private static final long serialVersionUID = 1L;
-    public double Frequency = 0;
+public class Indexer{
+    public ArrayList<index_double> Frequency = new ArrayList<>();
     public Indexer[] children = new Indexer[26];
 
 }
+
+class utility{
+    public static int binarySearch(ArrayList<index_double> Frequency, int bookIndex){
+        int l = 0, r = Frequency.size(), lr;
+
+        while (r > l + 1){
+            lr = (l + r) / 2;
+
+            if (Frequency.get(lr).bookIndex <= bookIndex)
+                l = lr;
+            else
+                r = lr;
+        }
+        lr = (l + r) / 2;
+
+        if (Frequency.get(lr).bookIndex == bookIndex) return lr;
+        else if (Frequency.get(l).bookIndex == bookIndex) return l;
+
+        return -1;
+    }
+}
+class index_double implements Serializable {
+    private static final long serialVersionUID = 1L;
+    int bookIndex;
+    double value;
+    public index_double(int index, double value){
+        this.value = value;
+        this.bookIndex = index;
+    }
+
+}
+
+
 class Trie {
     Indexer root = new Indexer();
-    double totalWords = 0;
+    ArrayList<Double> totalWords = new ArrayList<>();
 
-    public void insert(String word, double value) {
+    public void insert(String word, double value, int index) {
         Indexer node = root;
         for (char c : word.toCharArray()) {
             if (node.children[c - 'a'] == null) {
@@ -25,12 +56,18 @@ class Trie {
             }
             node = node.children[c - 'a'];
         }
-        node.Frequency += value;
-        if (value == 1)
-            totalWords++;
+        if (node.Frequency.size() == 0 || node.Frequency.get(node.Frequency.size() - 1).bookIndex != index )
+            node.Frequency.add(new index_double(index,0 ));
+        double ori = node.Frequency.get(node.Frequency.size() - 1).value;
+        node.Frequency.get(node.Frequency.size() - 1).value = ori + value;
+        if (value == 1){
+            if (index + 1 > totalWords.size())
+                totalWords.add(0.0);
+            totalWords.set(index, totalWords.get(index) + 1);
+        }
     }
 
-    public double search(String word) {
+    public double search(String word, int bookIndex) {
         Indexer node = root;
         
         for (char c : word.toCharArray()) {
@@ -39,69 +76,40 @@ class Trie {
                 return 0;
             }
         }
-        return node.Frequency;
+        int index = utility.binarySearch(node.Frequency, bookIndex);
+        if (index != -1)
+            return node.Frequency.get(index).value;
+        return 0;
     }
 
     public void dfs(Indexer node, String path){
-        
-        for (int i = 0;i < 26;i++) {
+        for (int i = 0;i < 26;i++){
             if (node.children[i] != null)
-                dfs(node.children[i], path + (char)(i + 97));
+                dfs(node.children[i], path + (char)(i +  (int)'a'));
         }
-        if (node.Frequency != 0){
-            try (DataOutputStream out = new DataOutputStream(new FileOutputStream(path + ".dat"))) {
-                out.writeDouble(node.Frequency);
-            } catch (IOException e) {
-                e.printStackTrace();
+
+        if (node.Frequency.size() != 0){
+            try (FileOutputStream fileOut = new FileOutputStream(path + ".ser");
+                ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+                out.writeObject(node.Frequency);
+            } catch (IOException i) {
+                i.printStackTrace();
             }
         }
-            
+    }
+
+    public void toSerial(String fileName){
+        File singleDir = new File(fileName);
+        if (!singleDir.exists()){
+            singleDir.mkdirs();
+        }
+        try (FileOutputStream fileOut = new FileOutputStream(fileName + "/_totalWords.ser");
+                ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+                out.writeObject(totalWords);
+            } catch (IOException i) {
+                i.printStackTrace();
+            }
+        dfs(root, fileName + "/");
     }
     
-    public void toSerial(String path){
-        File singleDir = new File(path);
-        if (!singleDir.exists())
-            singleDir.mkdir();
-        try (DataOutputStream out = new DataOutputStream(new FileOutputStream(path + "/_totalWord.dat"))) {
-            out.writeDouble(totalWords);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        dfs(root, path + "/");
-    }
-
-    public void deSerialWord(String word, String TrieName) {
-        /*
-         * Be careful, the word not exist will insert -1
-         */
-        StringBuilder sb = new StringBuilder();
-        sb.append("data_dir/");
-        sb.append(TrieName);
-        sb.append("/");
-        sb.append(word);
-        sb.append(".dat");
-
-        String fileName = sb.toString();
-
-        
-        try (DataInputStream in = new DataInputStream(new FileInputStream(fileName))) {
-            double value = in.readDouble();
-
-            insert(word, value);
-        } catch (IOException e) {
-            insert(word, -1);
-        }
-    }
-    public void deSerialTotal(String TrieName) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("data_dir/");
-        sb.append(TrieName);
-        sb.append("/_totalWord.dat");
-        String fileName = sb.toString();
-        try (DataInputStream in = new DataInputStream(new FileInputStream(fileName))) {
-            totalWords = in.readDouble();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
